64bf07c3fd2b18e3ef7c8e88337ba060d7033582
Ndala Tresor buzangu <ndalabuzangu@gmail.com>
2026-03-16 10:56:10 +0000

    Delete views directory

--- views/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header("Location: login.php?logout=true"); // Redirect to login with a logout notice
-exit;
-?>
